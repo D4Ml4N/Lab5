f415bfc0054ca768bbe6ff7410c99f5ed530cc6c
D4Ml4N <galkowski.damian@gmail.com>
2021-05-24 01:42:36 +0200

    Lab5/Zad4

--- src/pong1/Pong1.java
+++ src/pong1/Pong1.java
@@ -36,22 +36,28 @@ public class Pong1 extends Application {
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
-
-
         t.play();
     }
 
-
     private double x = ARENAX1 + ARENAWIDTH / 2;
-    
+    private double y = ARENAY1 + ARENAHEIGHT / 2;
+
+    private double vx = 5;
+    private double vy = 2;
 
     private void run(GraphicsContext gc) {
 
         gc.setFill(Color.BLACK);
         gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
 
+        if ((x <= ARENAX1) || ((x >= ARENAX2))) vx = -vx;
+        if ((y <= ARENAY1) || ((y >= ARENAY2))) vy = -vy;
+
+        x += vx;
+        y += vy;
+
         gc.setFill(Color.WHITESMOKE);
-        gc.fillOval(ARENAX1 + ARENAWIDTH / 2, ARENAY1 + ARENAHEIGHT / 2, 2 * R, 2 * R);
+        gc.fillOval(x, y, 2*R, 2*R);
     }
 
 
